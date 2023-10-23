895096bd80fd44fe4f0ab5b562c6be7c4db5fecc
JG-Stadler <joaogbstadler@gmail.com>
2023-10-23 15:19:21 -0300

    Menu Mobile finalizado
    Header responsivo e Menu Hamburger implementados

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import NavBar from './navBar'
 import Logo from "../images/logo.svg"
+import '../mobile-menu.css'
 
 export default function Header(){
     return(
         <header className="header-page">
-                <div>
                     <img src={Logo} alt="logo" className="logo"/>
+
                     <NavBar/>
                     
-                                <section className="apresentation-text">
-                        <h1>Immersive
-                        <br/>experiences
-                        <br/>that deliver
-                        </h1>
-                                </section>
-                </div>
+                <section className="apresentation-text">
+                    <h1>Immersive
+                    <br/>experiences
+                    <br/>that deliver
+                    </h1>
+                </section>
         </header>
     )
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 
 export default function NavBar(){
-   const [activeMenu,activateMenu] = useState(false)
+   const [ButtonClass,SetButtonClass] = useState("menu-button")
+   const [NavClass,SetNavClass] = useState("nav-bar")
+   const body = document.querySelector("body");
+
+
     return(
-    <nav className="nav-bar">
+        <>
+    <nav className={NavClass}>
         <ul className="nav-list">
                <li className="nav-item"><a href="#" className="nav-link">About</a></li>
                <li className="nav-item"><a href="#" className="nav-link">Careers</a></li>
@@ -11,13 +16,25 @@ export default function NavBar(){
                <li className="nav-item"><a href="#" className="nav-link">Products</a></li>
                <li className="nav-item"><a href="#" className="nav-link">Support</a></li> 
         </ul>
-
-            <div className="menu-button">
-                <span className="menu-button-bar"></span>
-                <span className="menu-button-bar"></span>
-                <span className="menu-button-bar"></span>
-            </div>
-
     </nav>
+
+    
+            <div className={ButtonClass}
+            onClick={()=>{         
+                body.style.overflow="hidden";
+                
+                if(ButtonClass === "menu-button"){
+                    SetButtonClass("menu-button menu-button-active");
+                    SetNavClass("nav-bar nav-bar-active");
+                }else{
+                    SetButtonClass("menu-button");
+                    SetNavClass("nav-bar");
+                }
+            }}>
+                <span id="bar1"></span>
+                <span id="bar2"></span>
+                <span id="bar3"></span>
+            </div>
+</>
     )
 }
